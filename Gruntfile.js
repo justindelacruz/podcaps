aa9c201580f7dcabd831da683659f2ab5f6d9c1f
Justin Dela Cruz <justindelacruz@server.fake>
2015-06-01 23:53:37 -0700

    Add Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        compass: {
+            dist: {
+                options: {
+                    specify: 'src/static/sass/app.scss'
+                }
+            }
+        },
+        bower: {
+            dev: {
+                dest: 'src/static/bower_components'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-bower');
+
+    // Default tasks
+    grunt.registerTask('default', ['compass']);
+};
