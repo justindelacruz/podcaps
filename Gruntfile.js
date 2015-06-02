ff243ee97d3906845d8095696567fa6de58a22ed
Justin Dela Cruz <justindelacruz@server.fake>
2015-06-02 01:01:09 -0700

    Clean up

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,5 +24,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-bower');
 
     // Default tasks
-    grunt.registerTask('heroku:production', ['compass', 'bower']);
+    grunt.registerTask('heroku:production', ['bower', 'compass']);
 };
