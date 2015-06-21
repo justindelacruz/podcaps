66b3bafd0635812d19c5053f30fca152d54ece6b
Justin Dela Cruz <justindelacruz@server.fake>
2015-06-21 16:06:56 -0700

    Configure Karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,33 +1,39 @@
+"use strict";
+
 module.exports = function(config){
-  config.set({
+    config.set({
 
-    basePath : './',
+        basePath : './',
 
-    files : [
-      'src/static/bower_components/angular/angular.js',
-      'src/static/bower_components/angular-route/angular-route.js',
-      'src/static/bower_components/angular-mocks/angular-mocks.js',
-      'src/static/ng-app/**/*.js',
-      'src/static/ng-app/view*/**/*.js'
-    ],
+        files : [
+            'src/static/bower_components/jquery/dist/jquery.js',
+            'src/static/bower_components/foundation/js/foundation.js',
+            'src/static/bower_components/angular/angular.js',
+            'src/static/bower_components/angular-route/angular-route.js',
+            'src/static/bower_components/angular-resource/angular-resource.js',
+            'src/static/bower_components/angular-sanitize/angular-sanitize.js',
+            'src/static/bower_components/angular-animate/angular-animate.js',
+            'src/static/bower_components/angular-mocks/angular-mocks.js',
+            'src/static/ng-app/**/*.js'
+        ],
 
-    autoWatch : true,
+        autoWatch : true,
 
-    frameworks: ['jasmine'],
+        frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+        browsers : ['Chrome'],
 
-    plugins : [
+        plugins : [
             'karma-chrome-launcher',
             'karma-firefox-launcher',
             'karma-jasmine',
             'karma-junit-reporter'
-            ],
+        ],
 
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
+        junitReporter : {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        }
 
-  });
+    });
 };
