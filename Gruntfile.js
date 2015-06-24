27eb804e2146c66ec444e1b50cce3b689a1a3447
Justin Dela Cruz <justindelacruz@server.fake>
2015-06-23 23:04:00 -0700

    Adding grunt concat and html2js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,91 @@
-module.exports = function(grunt) {
+"use strict";
 
-    // Project configuration.
+module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
         compass: {
             dist: {
                 options: {
                     specify: 'src/static/sass/app.scss'
                 }
             }
+        },
+
+        html2js: {
+            dist: {
+                src: [ 'src/static/ng-app/*.tpl.html' ],
+                dest: 'tmp/templates.js'
+            }
+        },
+
+        concat: {
+            options: {
+                separator: ';'
+            },
+            // Compile modules before everything else
+            jsCore: {
+                src: [
+                    'src/static/ng-app/**/*.js',
+                    'tmp/*.js',
+                    '!src/static/ng-app/**/*.module.js',
+                    '!src/static/ng-app/**/*.spec.js'
+                ],
+                dest: 'tmp/jsCore.js'
+            },
+            jsApp: {
+                src: [
+                    'src/static/ng-app/**/*.module.js',
+                    'tmp/jsCore.js'
+                ],
+                dest: 'src/static/js/dist.js'
+            }
+        },
+
+        clean: {
+            temp: {
+                src: [ 'tmp' ]
+            }
+        },
+
+        uglify: {
+            dist: {
+                files: {
+                    'src/static/ng-app/dist.js': [ 'src/static/ng-app/dist.js' ]
+                },
+                options: {
+                    mangle: false
+                }
+            }
+        },
+
+        watch: {
+            dev: {
+                files: [ 'Gruntfile.js', 'src/static/ng-app/**/*.js', 'src/static/ng-app/**/*.tpl.html' ],
+                tasks: [ 'html2js:dist', 'concat:jsCore', 'concat:jsApp', 'clean:temp' ],
+                options: {
+                    atBegin: true
+                }
+            },
+            min: {
+                files: [ 'Gruntfile.js', 'src/static/ng-app/**/*.js', 'src/static/ng-app/**/*.tpl.html' ],
+                tasks: [ 'html2js:dist', 'concat:jsApp', 'concat:jsCore', 'clean:temp', 'uglify:dist' ],
+                options: {
+                    atBegin: true
+                }
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-html2js');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    // Default tasks
+    grunt.registerTask('dev', [ 'watch:dev' ]);
+    grunt.registerTask('test', [ 'karma:continuous' ]);
+    grunt.registerTask('minified', [ 'watch:min' ]);
     grunt.registerTask('heroku:production', ['compass']);
 };
